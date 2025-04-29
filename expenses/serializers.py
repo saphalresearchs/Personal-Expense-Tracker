3812eebb93f34230f806d1ddce7f5784693cbf24
Saphalya Acharya <programmersaphal@gmail.com>
2025-04-29 07:56:46 +0545

    Authentication done

---
+++ expenses/serializers.py
@@ -0,0 +1,19 @@
+# expenses/serializers.py
+
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password']
+        )
+        return user
--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# expenses/views.py
 
-# Create your views here.
+from rest_framework import generics
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+from rest_framework.permissions import AllowAny
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
