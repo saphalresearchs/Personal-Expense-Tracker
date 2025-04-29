60ebe48ede64fe71d07c441cdc2aff888d9250b2
Saphalya Acharya <programmersaphal@gmail.com>
2025-04-29 08:06:29 +0545

    Expense model done

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+# expenses/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Expense, ExpenseCategory
+
+admin.site.register(ExpenseCategory)
+admin.site.register(Expense)
--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+# expenses/models.py
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class ExpenseCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='expenses')
+    category = models.ForeignKey(ExpenseCategory, on_delete=models.SET_NULL, null=True, related_name='expenses')
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True)
+    date = models.DateField()
+
+    def __str__(self):
+        return f'{self.user.username} - {self.category.name} - {self.amount}'
